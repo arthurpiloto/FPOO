b44af3f30200865ce1ae04bde2efce67a21f2bd2
ARTHUR PILOTO SILVA <22102321@CFP127ALUNO.LOCAL>
2022-02-24 08:46:29 -0300

    exercício 5

---
+++ lista-de-ex2/modo-grafico/Teste5.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+class Teste5{
+    public static void main(String args[]){
+        JOptionPane jp = new JOptionPane();
+
+        double valorDolar = Double.parseDouble(jp.showInputDialog(null, "Informe o valor a ser convertido: "));
+        double cotacaoDolar = Double.parseDouble(jp.showInputDialo(null, "Informe a cotacao do dolar atualmente: "));
+
+        double real = valorDolar * cotacaoDolar;
+
+        jp.showMessageDialog(null, "US$" + valorDolar + " = R$" + real);
+    }
+}
