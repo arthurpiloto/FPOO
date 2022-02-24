7e36247b4e7558d11bdd91470816e56346f5098d
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-02-24 20:29:03 -0300

    exercício 6

---
+++ lista-de-ex2/modo-grafico/Teste6.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+class Teste6{
+    public static void main(String args[]){
+        JOptionPane jp = new JOptionPane();
+
+        double graus = Double.parseDouble(jp.showInputDialog(null, "Informe os graus em Fahrenheit: "));
+
+        double f = graus * 9 / 5 + 160;
+
+        jp.showMessageDialog(null, graus + "° Celsisus = " + f + "° Fahrenheit");
+    }
+}
