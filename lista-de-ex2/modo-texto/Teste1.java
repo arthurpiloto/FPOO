8f251034013f1249167efe062d700c0ed41c1d12
ARTHUR PILOTO SILVA <22102321@CFP127ALUNO.LOCAL>
2022-02-25 08:22:12 -0300

    add exercícios

---
+++ lista-de-ex2/modo-texto/Teste1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Teste1{
+    public static void main(String args[]){
+        Scanner sc = new Scanner();
+
+        System.out.print("\nDigite um numero: ");
+        String n1 = sc.nextInt();
+        System.out.print("\nDigite outro numero");
+        String n2 = sc.nextInt();
+
+        double soma = n1 + n2;
+        double sub = n1 - n2;
+        double multip = n1 * n2;
+        double divi = n1 / n2;
+
+        System.out.println("\nSoma = " + soma);
+        System.out.println("\nSubtracao = " + sub);
+        System.out.println("\nMultiplicacao = " + multip);
+        System.out.print("\nDivisao = " + divi);
+    }
+}
---
+++ lista-de-ex2/modo-texto/Teste2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Teste2{
+    public static void main(string args[]){
+        Scanner sc = new Scanner();
+
+        System.out.print("\nDigite um numero inteiro: ");
+        String n1 = sc.nextInt();
+        System.out.print("\nDigite outro numero inteiro: ");
+        String n2 = sc.nextInt();
+
+        double pot1 = Math.pow(n1, 5);
+        double pot2 = Math.pow(n2, 5);
+
+        System.out.println()
+    }
+}
