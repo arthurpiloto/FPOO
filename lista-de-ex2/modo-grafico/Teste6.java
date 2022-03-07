ae695385740e9dbd5886bbf67535a5f57c9b960e
ARTHUR PILOTO SILVA <22102321@CFP127ALUNO.LOCAL>
2022-03-07 11:24:46 -0300

    teste 6

--- lista-de-ex2/modo-grafico/Teste6.java
+++ lista-de-ex2/modo-grafico/Teste6.java
@@ -3,9 +3,9 @@ class Teste6{
     public static void main(String args[]){
         JOptionPane jp = new JOptionPane();
 
-        double graus = Double.parseDouble(jp.showInputDialog(null, "Informe os graus em Fahrenheit: "));
+        double graus = Double.parseDouble(jp.showInputDialog(null, "Informe os graus em Celsius: "));
 
-        double f = graus * 9 / 5 + 160;
+        double f = (graus * 9 / 5) + 32;
 
         jp.showMessageDialog(null, graus + "° Celsius = " + f + "° Fahrenheit");
     }
