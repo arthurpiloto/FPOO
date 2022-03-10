17896533e862d88b58083de7a25e768926b1db35
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-03-09 21:50:38 -0300

    estrutura simples

---
+++ estrutura-de-decisao/modo-grafico/Simples.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+class Simples{
+    public static void main(String args[]){
+        JOptionPane jp = new JOptionPane();
+
+        int idade = Integer.parseInt(jp.showInputDialog(null, "Informe sua idade: "));
+
+        if (idade > 17){
+            jp.showMessageDialog(null, "Se aliste!")
+        }
+    }
+}
