f4d03855eb45ddec118fa9734c0f824881f6c99c
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-03-16 10:30:37 -0300

    exercício 8

---
+++ lista-de-ex3/Teste8.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class Teste8{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nInforme seu peso: ");
+        double peso = sc.nextDouble();
+
+        System.out.print("Informe sua altura: ");
+        double altura = sc.nextDouble();
+
+        double imc = peso / (altura * altura);
+        System.out.printf("\nIMC = %.2f", imc);
+
+        if(imc < 17){
+            System.out.print("\nMuito abaixo do peso");
+        }
+        if(imc >= 17 && imc < 18.49){
+            System.out.print("\nAbaixo do peso");
+        }
+        if(imc >= 18.5 && imc < 24.99){
+            System.out.print("\nPeso normal");
+        }
+        if(imc >= 25 && imc < 29.99){
+            System.out.print("\nAcima do peso");
+        }
+        if(imc >= 30 && imc < 34.99){
+            System.out.print("\nObesidade I");
+        }
+        if(imc >= 35 && imc < 39.99){
+            System.out.print("\nObesidade II (severa)");
+        }
+        if(imc >= 40){
+            System.out.print("\nObesidade III (morbida)");
+        }
+    }
+}
