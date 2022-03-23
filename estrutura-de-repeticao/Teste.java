9fb5e6d14e8d9f63d205c290ed5c8a463722ddea
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-03-23 09:34:26 -0300

    início estrutura de decisão

---
+++ estrutura-de-repeticao/Teste.java
@@ -0,0 +1,17 @@
+class Teste{
+    public static void main(String args[]){
+        int n = 7;
+
+        System.out.print("\n" + n + " X 0 = " + (n * 0));
+        System.out.print("\n" + n + " X 1 = " + (n * 1));
+        System.out.print("\n" + n + " X 2 = " + (n * 2));
+        System.out.print("\n" + n + " X 3 = " + (n * 3));
+        System.out.print("\n" + n + " X 4 = " + (n * 4));
+        System.out.print("\n" + n + " X 5 = " + (n * 5));
+        System.out.print("\n" + n + " X 6 = " + (n * 6));
+        System.out.print("\n" + n + " X 7 = " + (n * 7));
+        System.out.print("\n" + n + " X 8 = " + (n * 8));
+        System.out.print("\n" + n + " X 9 = " + (n * 9));
+        System.out.println("\n" + n + " X 10 = " + (n * 10));
+    }
+}
