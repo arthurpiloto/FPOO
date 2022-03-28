e006c298df8eb430aa99c409d07389e744275b18
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-03-28 08:33:14 -0300

    ajustando erro

--- estrutura-de-repeticao/Teste.java
+++ estrutura-de-repeticao/Teste.java
@@ -3,7 +3,7 @@ class Teste{
         int n = 7;
 
         for (int i=0; i <=10; i++){
-            System.out.print("\n" + n + " X " + i + " = " + i);
+            System.out.print("\n" + n + " X " + i + " = " + i*n);
         }
     }
 }
