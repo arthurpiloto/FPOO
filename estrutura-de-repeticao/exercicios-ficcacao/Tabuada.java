aaf81d7c74e3c0f199f2bd54f9a4be9cf6d2febc
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-03-28 08:34:48 -0300

    exercício de fixação Tabuada

---
+++ estrutura-de-repeticao/exercicios-ficcacao/Tabuada.java
@@ -0,0 +1,23 @@
+// Elaborar um programa em Java que exibe na tela a tabuada de um número e dos dois números posteriores.
+
+class Tabuada{
+    public static void main(String args[]){
+        int n = 5;
+        for (int i=0; i <=10; i++){
+            System.out.print("\n" + n + " X " + i + " = " + i*n);
+        }
+        System.out.println("\n--------------------------------------------------------------------------------------------------------------------");
+
+        n++;
+        for (int i=0; i <=10; i++){
+            System.out.print("\n" + n + " X " + i + " = " + i*n);
+        }
+
+        System.out.println("\n-------------------------------------------------------------------------------------------------------------------");
+
+        n++;
+        for (int i=0; i <=10; i++){
+            System.out.print("\n" + n + " X " + i + " = " + i*n);
+        }
+    }
+}
