03ea00c24d62ca42b73b1a04f00e72d63fbc6a73
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-03-30 09:45:20 -0300

    teste while

---
+++ estrutura-de-repeticao/while/Teste.java
@@ -0,0 +1,10 @@
+class Teste{
+    public static void main(String args[]){
+        int i = 1;
+
+        while(i <= 10){
+            System.out.print("\n" + i);
+            i++;
+        }
+    }
+}
