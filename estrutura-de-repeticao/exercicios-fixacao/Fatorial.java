7ae2fe89181ab42e32d94852784e4aca9966382b
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-03-30 08:39:14 -0300

    exercício fatorial 1

--- estrutura-de-repeticao/exercicios-fixacao/Fatorial.java
+++ estrutura-de-repeticao/exercicios-fixacao/Fatorial.java
@@ -1,11 +1,14 @@
 // Elaborar um programa que exibe na tela o fatorial de um número.
+// O exercício funciona apenas até o número 12, por conta do número ultrapassar o limite da variável int.
 
 class Fatorial{
     public static void main(String args[]){
-        int n = 5;
+        int n = 12;
+        int fat = n;
 
-        for (int i=4; i>=1; --i){
-            
+        for (int i=n-1; i>=2; --i){
+            fat = fat * i;
         }
+        System.out.print("\n" + n + "! = " + fat);
     }
 }
