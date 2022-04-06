5bf741ad28dbb42acd0ebb80a547f997d120d298
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-04-06 11:16:32 -0300

    exercício 6

---
+++ lista-de-ex4/Unicode.java
@@ -0,0 +1,16 @@
+class Unicode{
+    public static void main(String args[]){
+        char ch = '\u2588';
+        for(int i = 1; i <= 51; i++){
+            System.out.print(ch);
+        }
+
+        System.out.print("\n" + ch);
+        System.out.print("    Desenvolvimento de Sistemas - Turma DS1-T    ");
+        System.out.println(ch);
+
+        for(int i = 1; i <= 51; i++){
+            System.out.print(ch);
+        }
+    }
+}
