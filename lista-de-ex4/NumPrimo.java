c754c414be6b1cd7cf1749e2f09819b295408a96
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-04-11 11:14:52 -0300

    Número Primo

--- lista-de-ex4/NumPrimo.java
+++ lista-de-ex4/NumPrimo.java
@@ -2,9 +2,15 @@ import java.util.Scanner;
 class NumPrimo{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
+        int n = 0;
 
-        System.out.print("\nDigite um numero inteiro >= 2: ");
-        int n = sc.nextInt();
+        while(true){
+            System.out.print("\nDigite um numero inteiro >= 2: ");
+            n = sc.nextInt();
+            if(n >= 2){
+                break;
+            }
+        }
 
         for(int i = 2; i<n/2+1; i++){
             if(n % i == 0){
