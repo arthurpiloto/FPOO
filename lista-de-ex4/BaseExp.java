84a7eaef44c6588a3d0eeddcbd650bd63b689aac
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-04-13 07:53:17 -0300

    ajustando exercício de Base e Expoente

--- lista-de-ex4/BaseExp.java
+++ lista-de-ex4/BaseExp.java
@@ -3,15 +3,23 @@ class BaseExp{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Base: ");
-        long base = sc.nextLong();
-        System.out.print("Expoente: ");
-        long exp = sc.nextLong();
-        long contador = base;
+        long base = 0;
+        long exp = 0;
 
+        while(true){
+            System.out.print("Base: ");
+            base = sc.nextLong();
+            System.out.print("Expoente: ");
+            exp = sc.nextLong();
+            if(base >= 0 && exp >= 0){
+                break;
+            }
+        }
+        
+        long contador = base;
         for(long i = exp-1; i >= 1; i--){
             contador *= base;
         }
-        System.out.print("\n= " + contador);
+        System.out.print(base + "^" + exp + " = " + contador);
     }
-}   
+}
