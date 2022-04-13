ac182512f56ee684bf3c1a22d39e788472179d30
Arthur Piloto <arthurpiloto.github@gmail.com>
2022-04-13 11:12:01 -0300

    exercício criando métodos

---
+++ CriarMetod.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+class CriarMetod{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("\nInsira um numero decimal: ");
+        double n1 = sc.nextDouble();
+        System.out.print("\nInsira outro numero decimal: ");
+        double n2 = sc.nextDouble();
+
+        double soma = somar(n1, n2);
+        double sub = subtrair(n1, n2);
+        double divi = dividir(n1, n2);
+        double multip = multiplicar(n1, n2);
+
+        System.out.println("\n------Soma------");
+        System.out.println(n1 + " + " + n2 + " = " + soma);
+        System.out.println("----------------");
+        
+        System.out.println("\n------Subtracao------");
+        System.out.println(n1 + " - " + n2 + " = " + sub);
+        System.out.println("---------------------");
+
+        System.out.println("\n------Multiplicacao------");
+        System.out.println(n1 + " * " + n2 + " = " + multip);
+        System.out.println("-------------------------");
+
+        if(n1 == 0 || n2 == 0){
+            System.out.println("\n------Divisao------");
+            System.out.println(n1 + " / " + n2 + " = 0");
+            System.out.println("-------------------");
+            System.exit(0);
+        }
+        System.out.println("\n------Divisao------");
+        System.out.println(n1 + " / " + n2 + " = " + divi);
+        System.out.println("-------------------");
+    }
+
+    public static double somar(double n1, double n2){
+        return(n1 + n2);
+    }
+
+    public static double subtrair(double n1, double n2){
+        return(n1 - n2);
+    }
+
+    public static double dividir(double n1, double n2){
+        return(n1 / n2);
+    }
+
+    public static double multiplicar(double n1, double n2){
+        return(n1 * n2);
+    }
+}
